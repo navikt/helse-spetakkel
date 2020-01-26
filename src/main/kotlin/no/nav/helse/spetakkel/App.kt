8776644344d6f0b2caedbf022eb3c67f62e50db5
David Steinsland <125661+davidsteinsland@users.noreply.github.com>
2020-01-26 11:03:55 +0100

    fikser mutable map

--- src/main/kotlin/no/nav/helse/spetakkel/App.kt
+++ src/main/kotlin/no/nav/helse/spetakkel/App.kt
@@ -3,7 +3,7 @@ package no.nav.helse.spetakkel
 import no.nav.helse.rapids_rivers.AppBuilder
 
 fun main() {
-    val env = System.getenv()
+    val env = System.getenv().toMutableMap()
     env.putIfAbsent("KAFKA_CONSUMER_GROUP_ID", "spetakkel-v1")
     env.putIfAbsent("KAFKA_RAPID_TOPIC", "privat-helse-sykepenger-rapid-v1")
 
