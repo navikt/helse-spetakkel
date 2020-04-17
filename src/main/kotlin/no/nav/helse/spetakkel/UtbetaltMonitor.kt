0d521978ddde188022c6ba758ec693d2c89ee6ba
Jakob Havstein Eriksen <jakob.havstein.eriksen@nav.no>
2020-04-17 15:45:45 +0200

    Utbetaling-eventet har fått nytt format
    Co-authored-by: Christian Bülow Skovborg <christian.bulow.skovborg@nav.no>

--- src/main/kotlin/no/nav/helse/spetakkel/UtbetaltMonitor.kt
+++ src/main/kotlin/no/nav/helse/spetakkel/UtbetaltMonitor.kt
@@ -3,10 +3,7 @@ package no.nav.helse.spetakkel
 import com.fasterxml.jackson.databind.JsonNode
 import io.prometheus.client.Counter
 import io.prometheus.client.Summary
-import no.nav.helse.rapids_rivers.JsonMessage
-import no.nav.helse.rapids_rivers.RapidsConnection
-import no.nav.helse.rapids_rivers.River
-import no.nav.helse.rapids_rivers.asLocalDate
+import no.nav.helse.rapids_rivers.*
 import java.time.DayOfWeek
 
 internal class UtbetaltMonitor(rapidsConnection: RapidsConnection) : River.PacketListener {
@@ -26,20 +23,17 @@ internal class UtbetaltMonitor(rapidsConnection: RapidsConnection) : River.Packe
     init {
         River(rapidsConnection).apply {
             validate { it.requireValue("@event_name", "utbetalt") }
-            validate { it.requireArray("utbetaling") {
-                requireArray("utbetalingslinjer") {
-                    requireKey("dagsats")
-                    require("fom", JsonNode::asLocalDate)
-                    require("tom", JsonNode::asLocalDate)
-                }
+            validate { it.requireArray("utbetalingslinjer") {
+                requireKey("dagsats")
+                require("fom", JsonNode::asLocalDate)
+                require("tom", JsonNode::asLocalDate)
             } }
         }.register(this)
     }
 
     override fun onPacket(packet: JsonMessage, context: RapidsConnection.MessageContext) {
         utbetaltCounter.inc()
-        val totalbeløp = packet["utbetaling"]
-            .flatMap { it["utbetalingslinjer"].toList() }
+        val totalbeløp = packet["utbetalingslinjer"]
             .map {
                 it["fom"].asLocalDate().datesUntil(it["tom"].asLocalDate().plusDays(1)).filter {
                     it.dayOfWeek !in listOf(DayOfWeek.SATURDAY, DayOfWeek.SUNDAY)
