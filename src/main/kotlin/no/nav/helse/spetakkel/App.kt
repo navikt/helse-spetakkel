cdcf3df3803e22f5561658f6739d165c517d1e68
David Steinsland <david@steinsland.email>
2020-04-28 08:33:43 +0200

    teller meldinger i aktivitetslogg

--- src/main/kotlin/no/nav/helse/spetakkel/App.kt
+++ src/main/kotlin/no/nav/helse/spetakkel/App.kt
@@ -13,6 +13,7 @@ fun main() {
 
     RapidApplication.create(env).apply {
         EventMonitor(this)
+        AktivitetsloggMonitor(this)
         TilstandsendringMonitor(this, TilstandsendringMonitor.VedtaksperiodeTilstandDao(dataSource))
         TidITilstandMonitor(this)
         VedtaksperiodePåminnetMonitor(this)
