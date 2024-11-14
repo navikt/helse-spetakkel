ce8896cb4116156058ebb9f86fe4aab68c92b4c9
david steinsland <david@davidsteinsland.net>
2024-11-14 09:36:49 +0100

    bumper tbd-libs

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,5 @@
 val junitJupiterVersion = "5.11.3"
-val tbdLibsVersion = "2024.11.12-20.46-e1fd6a25"
+val tbdLibsVersion = "2024.11.14-07.56-63a8564f"
 val rapidsAndRiversVersion = "2024111408081731568123.6619b48b7043"
 val mainClass = "no.nav.helse.spetakkel.AppKt"
 
