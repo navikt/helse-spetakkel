bcf96768be38555469f509a90c18a829ff2673c4
david steinsland <david@davidsteinsland.net>
2025-08-16 14:03:45 +0200

    bumper r&r

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,6 @@
 val junitJupiterVersion = "5.12.1"
 val tbdLibsVersion = "2025.08.16-09.21-71db7cad"
-val rapidsAndRiversVersion = "2025080710011754553680.051be9b54ef9"
+val rapidsAndRiversVersion = "2025081612341755340488.ff2c2d01e04f"
 val mainClass = "no.nav.helse.spetakkel.AppKt"
 val hikariCPVersion = "6.3.0"
 val flywayVersion = "11.5.0"
