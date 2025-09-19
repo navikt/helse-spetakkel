080b360c9c80fb765ed52b2a828401cd58e24c3c
Amalie Mansåker <amalie.erdal.mansaker@nav.no>
2025-09-19 13:59:01 +0200

    Bumper tbd-libs
    Co-authored-by: David Steinsland <david@davidsteinsland.net>
    Co-authored-by: Marte Tårnes <marte.tarnes@nav.no>

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,5 @@
 val junitJupiterVersion = "5.12.1"
-val tbdLibsVersion = "2025.08.16-09.21-71db7cad"
+val tbdLibsVersion = "2025.09.19-13.31-61342e73"
 val rapidsAndRiversVersion = "2025081612341755340488.ff2c2d01e04f"
 val mainClass = "no.nav.helse.spetakkel.AppKt"
 val hikariCPVersion = "6.3.0"
